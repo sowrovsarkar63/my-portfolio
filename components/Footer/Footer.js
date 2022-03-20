2fcda8704b31ed9dfd397442103cd975f23fcc15
sowrovsarkar63@gmail.com <dev.sowrovsarkar63@gmail.com>
2022-03-20 22:34:30 +0600

    start desiging portfolio website

---
+++ components/Footer/Footer.js
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <>
+      <h2 className=" bg-gray-700 ">footer</h2>
+    </>
+  );
+}
---
+++ components/Navbar/Navbar.js
@@ -0,0 +1,7 @@
+export default function Navbar() {
+  return (
+    <>
+      <h2 className=" bg-red-700 ">Navbar</h2>
+    </>
+  );
+}
