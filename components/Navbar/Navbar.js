2f310a9078482ddf857475ce888d6e788c887fb9
sowrovsarkar63@gmail.com <dev.sowrovsarkar63@gmail.com>
2022-03-21 01:07:39 +0600

    basic navbar structure added

--- components/Navbar/Navbar.js
+++ components/Navbar/Navbar.js
@@ -1,7 +1,31 @@
 export default function Navbar() {
   return (
     <>
-      <h2 className=" bg-red-700 ">Navbar</h2>
+      <nav className="navbar bg-base-100">
+        <div className="container mx-auto px-14">
+          <div className="flex-1">
+            <a href="#" className="btn btn-ghost normal-case text-xl">
+              SowrovSarkar
+            </a>
+          </div>
+          <div className="flex-none">
+            <ul className="menu menu-horizontal p-0">
+              <li>
+                <a href="#">Home</a>
+              </li>
+              <li>
+                <a href="#">About</a>
+              </li>
+              <li>
+                <a href="#">Portfolio</a>
+              </li>
+              <li>
+                <a href="#">Hire</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
     </>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -14,7 +14,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      {/* Navigation bar  */}
+      {/* Nav bar   */}
       <Navbar />
 
       {/* Main Content */}
